c8cf94097e442c6b12db17c695e96b6efda51c7e
MakeHui <makehuir@gmail.com>
2017-01-13 14:10:00 +0800

    libgit2 demo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,29 +6,58 @@
 
 using namespace LibQGit2;
 
+/**
+ * Mac 上 libgit2 可以clone仓库
+ * windows 上的问题需要单独处理
+ * 基本可以确定是 windows 和 libgit2 兼容的问题
+ * @brief do_git_clone
+ * @param repo_directory
+ * @param url
+ */
+void do_git_clone(const char* repo_directory, const char* url) {
+  git_repository* repo;
+
+  git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
+
+  int rtn = git_clone(&repo, url, repo_directory, &opts);
+  if (rtn == 0) {
+    qDebug() <<  ("clone success\n");
+    git_repository_free(repo);
+  } else {
+    const git_error* err = giterr_last();
+    qDebug() << err->klass;
+    qDebug() << err->message;
+  }
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QString Url = "https://git.oschina.net/MakeHuiR/test.git";
-    QString Url2 = "http://git.famishare.cn/MakeHui/test2.git";
-    QString GitPath = "/Users/MakeHui/Desktop/test3";
-    QString UserName = "makehuir@gmail.com";
-    QByteArray Passphrase = "oschinaMail003";
+//    do_git_clone("/Users/MakeHui/Desktop/test3", "http://git.famishare.cn/MakeHui/test.git");
 
-    QByteArray UserName2 = "makehuir@gmail.com";
+    QString PrivateUrl = "http://git.famishare.cn/MakeHui/test2.git";
+    QString PublicUrl = "http://git.famishare.cn/MakeHui/test.git";
+
+    QString MacPath = "/Users/MakeHui/Desktop/test3";
+    QString WindowsPath = "C:/Users/MakeHui/Desktop/test3";
+
+    QString UserName = "2240544497@qq.com";
+    QString Passphrase = "yaoHui003";
 
     auto repository = new LibQGit2::Repository();
+
+//    try {
+//        auto credentials = Credentials::ssh(QString(), QString(), UserName, Passphrase);
+//        repository->setRemoteCredentials("origin", credentials);
+//    } catch (const LibQGit2::Exception& e) {
+//        qDebug() << e.message();
+//    }
+
     try {
-        auto credentials = Credentials::ssh(QString(), QString(), "2240544497@qq.com", "yaoHui003");
-        repository->setRemoteCredentials("origin", credentials);
-    } catch (const LibQGit2::Exception& e) {
-        qDebug() << e.message();
-    }
-    try {
-        repository->clone(Url2, GitPath);
+        repository->clone(PublicUrl, MacPath);
     } catch (const LibQGit2::Exception& e) {
         qDebug() << e.message();
     }
