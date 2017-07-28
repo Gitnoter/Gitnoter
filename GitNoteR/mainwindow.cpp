357124b1e7216301fb624cb0368d9efe8cec71e9
MakeHui <makehuir@gmail.com>
2017-07-28 20:44:01 +0800

    bak

--- GitNoteR/mainwindow.cpp
+++ GitNoteR/mainwindow.cpp
@@ -4,39 +4,72 @@
 #include <QtDebug>
 #include "qgit2.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QString PrivateUrl = "http://git.famishare.cn/MakeHui/test2.git";
-    QString PublicUrl = "http://git.famishare.cn/MakeHui/test.git";
 
-    QString MacPath = "/Users/MakeHui/Desktop/test3";
-    QString WindowsPath = "C:/Users/MakeHui/Desktop/test3";
-
-    QString UserName = "2240544497@qq.com";
-    QString Passphrase = "yaoHui003";
-
-    auto repository = new LibQGit2::Repository();
-
-//    try {
-//        auto credentials = Credentials::ssh(QString(), QString(), UserName, Passphrase);
-//        repository->setRemoteCredentials("origin", credentials);
-//    } catch (const LibQGit2::Exception& e) {
-//        qDebug() << e.message();
-//    }
-
-//    try {
-//        repository->clone(PublicUrl, MacPath);
-//    } catch (const LibQGit2::Exception& e) {
-//        qDebug() << e.message();
-//    }
-//    qDebug() << repository->listReferences();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString MacPath = "/Users/MakeHui/Desktop/test3";
+    QString WindowsPath = "C:/Users/MakeHui/Desktop/test3";
+
+    QString PublicUrl = "http://github.com/c9/c9.ide.ace.repl.git";
+    QString PrivateUrl = "http://git.oschina.net/make/test.git";
+
+    QByteArray UserName = "makehuir@gmail.com";
+    QByteArray Passphrase = "oschinaMail003";
+
+    auto repository = new LibQGit2::Repository();
+
+//    try {
+//        auto credentials = LibQGit2::Credentials::ssh(QString(), QString(), UserName, Passphrase);
+//        repository->setRemoteCredentials("origin", credentials);
+//    } catch (const LibQGit2::Exception& e) {
+//        qDebug() << e.message();
+//    }
+
+    try {
+        repository->clone(PublicUrl, MacPath);
+    } catch (const LibQGit2::Exception& e) {
+        qDebug() << e.message();
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString MacPath = "/Users/MakeHui/Desktop/test3";
+    QString WindowsPath = "C:/Users/MakeHui/Desktop/test3";
+
+    QString PublicUrl = "http://git.coding.net/sciooga/Keyboard-launch.git";
+    QString PrivateUrl = "http://git.oschina.net/make/test.git";
+    QString PrivateUrl2 = "http://git.coding.net/MakeHui/a40.git";
+
+    QByteArray UserName = "makehuir@gmail.com";
+    QByteArray Passphrase = "oschinaMail003";
+
+    auto repository = new LibQGit2::Repository();
+
+    try {
+        auto credentials = LibQGit2::Credentials::ssh(QString(), QString(), UserName, Passphrase);
+        repository->setRemoteCredentials("origin", credentials);
+    } catch (const LibQGit2::Exception& e) {
+        qDebug() << e.message();
+    }
+
+    try {
+        repository->clone(PrivateUrl, MacPath);
+    } catch (const LibQGit2::Exception& e) {
+        qDebug() << e.message();
+    }
 }
