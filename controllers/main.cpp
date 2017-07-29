4dd43e1725ea99d641594d845a1b165123e7d40a
MakeHui <makehuir@gmail.com>
2017-07-29 22:13:46 +0800

    add libgit2.dll for windows

--- controllers/main.cpp
+++ controllers/main.cpp
@@ -19,24 +19,25 @@ int main(int argc, char *argv[])
 
 
 //    GitManager *gitManager = new GitManager();
-    const char *url = "https://git.oschina.net/TerryZ/bTabs.git";
-    const char *repo_directory = "/Users/apple/Desktop/test3";
 
-    const char *purl = "https://git.oschina.net/make/libgit2-test.git";
-    const char *prepo_directory = "/Users/apple/Desktop/test2";
-
-    char *username = "makehuir@gmail.com";
-    char *password = "oschinaMail003";
-
-    QString docFolder = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
-    docFolder = QDir(docFolder).filePath("test3");
-
-    QByteArray array = docFolder.toLocal8Bit();
-    char* buffer = array.data();
-
-    qDebug() << buffer;
-    GitManager *gitManager = new GitManager(username, password);
-    gitManager->clone(purl, buffer);
+//    const char *url = "https://git.oschina.net/TerryZ/bTabs.git";
+//    const char *repo_directory = "/Users/apple/Desktop/test3";
+//
+//    const char *purl = "https://git.oschina.net/make/libgit2-test.git";
+//    const char *prepo_directory = "/Users/apple/Desktop/test2";
+//
+//    char *username = "makehuir@gmail.com";
+//    char *password = "oschinaMail003";
+//
+//    QString docFolder = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+//    docFolder = QDir(docFolder).filePath("test3");
+//
+//    QByteArray array = docFolder.toLocal8Bit();
+//    char* buffer = array.data();
+//
+//    qDebug() << buffer;
+//    GitManager *gitManager = new GitManager(username, password);
+//    gitManager->clone(purl, buffer);
 
 
     return app.exec();
