cfb3f849328cc049d870ac2bc88c46434fbf7361
MakeHui <makehuir@gmail.com>
2017-12-31 14:33:54 +0800

    release v1.0.0.beta3

--- version.h
+++ version.h
@@ -7,8 +7,8 @@
 #define VER_FILEVERSION_STR         "1.0.0.0"
 
 #define VER_PRODUCTVERSION          1,0,0,0
-#define VER_PRODUCTVERSION_STR      "1.0.0.0"   // 产品版本
-#define VER_PRODUCTBUILD_STR        1           // build
+#define VER_PRODUCTVERSION_STR      "1.0.0.beta3"   // 产品版本
+#define VER_PRODUCTBUILD_STR        131           // build
 
 #define VER_COMPANYNAME_STR         "Gitnoter"
 #define VER_INTERNALNAME_STR        "Gitnoter.exe"
@@ -17,7 +17,7 @@
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_PRODUCTNAME_STR         "Gitnoter"        // 产品名称
 #define VER_ORIGINALFILENAME_STR    "Gitnoter.exe"    // 原始文件名
-#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.0 (build 1)"    // 文件说明
+#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.beta3 (build 131)"    // 文件说明
 
 #define VER_ORGANIZATIONDOMAIN_STR  "http://gitnoter.com"
 
