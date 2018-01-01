a37ce0969ffd9fc1dadd9b3092a1a661369d1b70
MakeHui <makehuir@gmail.com>
2018-01-01 13:37:31 +0800

    add release.bat

---
+++ release-build/release.bat
@@ -0,0 +1,11 @@
+rd /s/q .\release\Gitnoter
+rd /s/q .\release\Gitnoter.zip
+md .\release\Gitnoter
+
+copy /y ..\build-Gitnoter-Desktop_Qt_5_9_3_MSVC2015_32bit-Release\release\Gitnoter.exe .\release\Gitnoter\
+
+windeployqt.exe .\release\Gitnoter\Gitnoter.exe --release
+
+xcopy .\window-missing-dll .\release\Gitnoter\ /s/f/h/y
+
+7z.exe a -tzip -r .\release\Gitnoter.zip .\release\Gitnoter\*
