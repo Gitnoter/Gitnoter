bfd336f3097723e080e2243ce36bd896645bc4f6
MakeHui <makehuir@gmail.com>
2018-01-01 17:51:02 +0800

    add release build script for linux

---
+++ release-build/release-linux.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+rm -rf ./release/Gitnoter
+mkdir -p ./release/Gitnoter
+cp -rf ../build-Gitnoter-Desktop_Qt_5_8_0_GCC_64bit-Release/Gitnoter ./release/Gitnoter/
+
+i=0
+cat ../src/version.h | while read line
+do
+    if [[ ${i} == 9 ]]; then
+        VERSION=`echo ${line} | sed 's/.*VER_PRODUCTVERSION_STR *"\([0-9.-beta]*\)".*/\1/g'`
+        linuxdeployqt ./release/Gitnoter/Gitnoter
+        cp ../src/3rdparty/libgit2/lib/libgit2.so ./release/Gitnoter/lib/libgit2.so.24
+        echo "app verson: ${VERSION}"
+        tar -czf ./release/Gitnoter-linux-${VERSION}.tar.gz ./release/Gitnoter
+    fi
+    let i=i+1
+done
