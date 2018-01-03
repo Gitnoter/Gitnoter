0acdc893ce43dc26425b8cc2d763816a2db8d502
MakeHui <makehuir@gmail.com>
2018-01-03 18:02:52 +0800

    update version info

--- src/version.h
+++ src/version.h
@@ -3,21 +3,22 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,0,0     //文件版本
-#define VER_FILEVERSION_STR         "1.0.0.0"
+#define VER_FILEVERSION             1,0,0,4     //文件版本
+#define VER_FILEVERSION_STR         "1.0.0.4"
 
-#define VER_PRODUCTVERSION          1,0,0,0
-#define VER_PRODUCTVERSION_STR      "1.0.0.beta3"   // 产品版本
-#define VER_PRODUCTBUILD_STR        131           // build
+#define VER_PRODUCTVERSION          1,0,0,4
+#define VER_PRODUCTVERSION_STR      "1.0.0.beta4"   // 产品版本
+#define VER_PRODUCTBUILD_STR        140           // build
 
 #define VER_COMPANYNAME_STR         "Gitnoter"
 #define VER_INTERNALNAME_STR        "Gitnoter.exe"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2017 MakeHui. "  // 版权
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2018 MakeHui. "  // 版权
 #define VER_LEGALTRADEMARKS1_STR    "All rights reserved. "
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_PRODUCTNAME_STR         "Gitnoter"        // 产品名称
 #define VER_ORIGINALFILENAME_STR    "Gitnoter.exe"    // 原始文件名
-#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.beta3 (build 131)"    // 文件说明
+
+#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.beta4 (build 140)"    // 文件说明
 
 #define VER_ORGANIZATIONDOMAIN_STR  "http://gitnoter.com"
 
