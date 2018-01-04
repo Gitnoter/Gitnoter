de56326723560630cbe1777a306ce937562bcc1f
MakeHui <makehuir@gmail.com>
2018-01-04 18:06:15 +0800

    fix Dyld Error Message: libcrypto.1.0.0.dylib bug

--- release-build/release-osx.sh
+++ release-build/release-osx.sh
@@ -10,6 +10,7 @@ do
     if [[ ${i} == 9 ]]; then
         VERSION=`echo ${line} | sed 's/.*VER_PRODUCTVERSION_STR *"\([0-9.-beta]*\)".*/\1/g'`
         macdeployqt ./release/Gitnoter.app
+        install_name_tool -change /usr/local/Cellar/openssl/1.0.2n/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib ./release/Gitnoter.app/Contents/Frameworks/libssl.1.0.0.dylib
         appdmg ./package-osx/package.json ./release/Gitnoter-osx-v${VERSION}.dmg
     fi
     let i=i+1
