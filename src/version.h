9812cd24f04ecc1fef1ec9d485b5fff1e92948b7
MakeHui <makehuir@gmail.com>
2018-01-07 01:17:00 +0800

    update version info to v1.0.0.beta5

--- src/version.h
+++ src/version.h
@@ -3,12 +3,12 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,0,4     //文件版本
-#define VER_FILEVERSION_STR         "1.0.0.4"
+#define VER_FILEVERSION             1,0,0,5     //文件版本
+#define VER_FILEVERSION_STR         "1.0.0.5"
 
 #define VER_PRODUCTVERSION          1,0,0,4
-#define VER_PRODUCTVERSION_STR      "1.0.0.beta4"   // 产品版本
-#define VER_PRODUCTBUILD_STR        140           // build
+#define VER_PRODUCTVERSION_STR      "1.0.0.beta5"   // 产品版本
+#define VER_PRODUCTBUILD_STR        152           // build
 
 #define VER_COMPANYNAME_STR         "Gitnoter"
 #define VER_INTERNALNAME_STR        "Gitnoter.exe"
@@ -18,7 +18,7 @@
 #define VER_PRODUCTNAME_STR         "Gitnoter"        // 产品名称
 #define VER_ORIGINALFILENAME_STR    "Gitnoter.exe"    // 原始文件名
 
-#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.beta4 (build 140)"    // 文件说明
+#define VER_FILEDESCRIPTION_STR     "Gitnoter for Windows Version 1.0.0.beta5 (build 152)"    // 文件说明
 
 #define VER_ORGANIZATIONDOMAIN_STR  "http://gitnoter.com"
 
